ef9e582f0202abddac0b792591d8255721a0b4c5
Muhammad Mahbub Sarwar Shafi <mahbubsarwar49@gmail.com>
2024-05-11 16:12:19 +0600

    each item get from the items collection

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config()
@@ -29,6 +29,13 @@ async function run() {
 
     app.get('/items', async(req, res)=>{
       const result = await foodsCollection.find().toArray();
+      res.send(result);
+    })
+
+    app.get('/items/:id', async(req, res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const result = await foodsCollection.findOne(query);
       res.send(result);
     })
 
