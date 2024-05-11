d9a3e92a113495cdb95df3258b520df4a1dcc4f4
Muhammad Mahbub Sarwar Shafi <mahbubsarwar49@gmail.com>
2024-05-11 14:06:26 +0600

    posting food items added by user in the foodscollection and get

--- index.js
+++ index.js
@@ -24,6 +24,20 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const foodsCollection = client.db('trioEats').collection("foods");
+
+    app.get('/items', async(req, res)=>{
+      const result = await foodsCollection.find().toArray();
+      res.send(result);
+    })
+
+    app.post('/items', async(req, res)=>{
+      const item = req.body;
+      const result = await foodsCollection.insertOne(item);
+      res.send(result);
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
