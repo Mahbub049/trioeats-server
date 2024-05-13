7f801ee2ec2c1cb037e78dbb9eae9eedc8f32ca4
Muhammad Mahbub Sarwar Shafi <mahbubsarwar49@gmail.com>
2024-05-13 19:54:12 +0600

    delete functionality implemented in my purchase page

--- index.js
+++ index.js
@@ -97,6 +97,13 @@ async function run() {
       res.send(result);
     })
 
+    app.delete('/purchases/:id', async(req, res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await purchaseCollection.deleteOne(query);
+      res.send(result)
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
