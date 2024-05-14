a7938b39b8668a2abf4baf89a473ca00a5dd6ef6
Muhammad Mahbub Sarwar Shafi <mahbubsarwar49@gmail.com>
2024-05-14 17:15:43 +0600

    jwt token generated and clearcookie implemented

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors({
+  origin: [
+    'http://localhost:5173'
+  ],
+  credentials: true
+}));
 app.use(express.json());
 
 
@@ -75,6 +81,23 @@ async function run() {
       };
       const result = await foodsCollection.find(query).toArray();
       res.send(result);
+    })
+
+    //JWT
+    app.post('/jwt', async(req, res)=>{
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '1hr'});
+      res
+      .cookie('token',token,{
+        httpOnly: true,
+        secure: true,
+        sameSite: 'none'
+      })
+      .send({success: true});
+    })
+
+    app.post('/logout', async(req, res)=>{
+      res.clearCookie('token', {maxAge: 0}).send({success: true})
     })
   
     app.post('/items', async(req, res)=>{
